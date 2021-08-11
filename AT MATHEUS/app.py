0a588b9bd9d0415cdd79108fd6ee7a58e76e1792
Guilherme Rodrigues Oliveira <62958414+1NxT@users.noreply.github.com>
2021-08-11 17:53:48 -0300

    Add

--- AT MATHEUS/app.py
+++ AT MATHEUS/app.py
@@ -1,8 +1,16 @@
 from flask import Flask, render_template, redirect, request
 from classes.conta import Conta
 
-
 app = Flask(__name__)
+
+
+conta1 = Conta(33334, "Guilherme Rodrigues Oliveira", 5000, 3000)
+conta2 = Conta(33333, "João Amorim", 5000, 3000)
+conta3 = Conta(33332, "Viktor da Silva", 5000, 3000)
+
+
+lista_conta = [conta1, conta2, conta3]
+
 
 @app.route('/')
 def index():
@@ -10,14 +18,21 @@ def index():
 
 @app.route('/listar')
 def listar():
-    return render_template('listar.html')
+    return render_template('listar.html', lista_conta=lista_conta)
 
 @app.route('/adicionar')
 def adicionar():
     return render_template('adicionar.html')
 
-@app.route('/adicionar/adicionado')
+@app.route('/adicionar/adicionado', method=["GET"])
 def adicionar():
-    return redirect('/')
+    nomeTitular = request.form.get('nomeTitular')
+    numeroConta = request.form.get('numeroConta')
+    saldo = request.form.get('saldo')
+    chequeEspecial = request.form.get('chequeEspecial')
+
+    novaConta = Conta(numeroConta, nomeTitular, saldo, chequeEspecial)
+    lista_conta.append(novaConta)
+    return redirect('/listar')
 
 app.run()
