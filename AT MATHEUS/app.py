338615fb085692f1e4dc6d107d1522353ba1311e
Guilherme Rodrigues Oliveira <62958414+1NxT@users.noreply.github.com>
2021-08-11 11:40:41 -0300

    Add

--- AT MATHEUS/app.py
+++ AT MATHEUS/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
 from classes.conta import Conta
 
 
@@ -7,5 +7,17 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/listar')
+def listar():
+    return render_template('listar.html')
+
+@app.route('/adicionar')
+def adicionar():
+    return render_template('adicionar.html')
+
+@app.route('/adicionar/adicionado')
+def adicionar():
+    return redirect('/')
 
 app.run()
