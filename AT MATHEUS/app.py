51f41478de0b19706cfe297e885d5b5b10454651
Guilherme Rodrigues Oliveira <62958414+1NxT@users.noreply.github.com>
2021-08-12 17:39:17 -0300

    END

--- AT MATHEUS/app.py
+++ AT MATHEUS/app.py
@@ -1,39 +1,43 @@
-# -*- coding: utf-8 -*-
 from flask import Flask, render_template, redirect, request
 from conta import Conta
 
 app = Flask(__name__)
 
 
-conta1 = Conta(33334, "Guilherme Rodrigues Oliveira", 5000, 3000)
-conta2 = Conta(33333, "João Amorim", 5000, 3000)
-conta3 = Conta(33332, "Viktor da Silva", 5000, 3000)
+conta1 = Conta(33334, "Guilherme", 5000, 3000)
+conta2 = Conta(33333, "Joao", 5000, 3000)
+conta3 = Conta(33332, "Viktor", 5000, 3000)
 
 
-lista_conta = [conta1, conta2, conta3]
+contas = [conta1, conta2, conta3]
+
+
+@app.route('/conta/cadastro')
+def adicionar():
+    return render_template('adicionar.html')
+
+
+@app.route('/listarcontas')
+def listarContas():
+    return render_template('listar.html',  lista_conta=contas)
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/listar')
-def listar():
-    return render_template('listar.html', lista_conta=lista_conta)
 
-@app.route('/adicionar')
-def adicionar():
-    return render_template('adicionar.html')
-
-@app.route('/adicionar/adicionado', method=["GET"])
-def adicionar():
-    nomeTitular = request.form.get('nomeTitular')
-    numeroConta = request.form.get('numeroConta')
-    saldo = request.form.get('saldo')
-    chequeEspecial = request.form.get('chequeEspecial')
+@app.route('/conta/cadastar', methods=["GET"])
+def cadastrado():
+    nomeTitular = request.args.get('nomeTitular')
+    numeroConta = request.args.get('numeroConta')
+    saldo = int(request.args.get('saldo'))
+    chequeEspecial = int(request.args.get('chequeEspecial'))
 
     novaConta = Conta(numeroConta, nomeTitular, saldo, chequeEspecial)
-    lista_conta.append(novaConta)
-    return redirect('/listar')
+    contas.append(novaConta)
+    return redirect('/')
+
+
 
 app.run()
